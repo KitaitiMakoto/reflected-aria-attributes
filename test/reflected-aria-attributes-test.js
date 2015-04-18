bace142460ddd9afa9ff05fd37c268bd585f666d
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-04-19 07:15:38 +0900

    Create element for every test to avoid other tests' effects

--- test/reflected-aria-attributes-test.js
+++ test/reflected-aria-attributes-test.js
@@ -8,15 +8,18 @@ var window = jsdom.jsdom().defaultView;
 var document = window.document;
 
 describe("ReflectedARIAAttributes.define()", () => {
-    var button = document.createElement("span");
-    ReflectedARIAAttributes.define(button, ["aria-pressed"]);
-
     it("should define reflected WAI-ARIA attributes to an element", () => {
+        var button = document.createElement("span");
+        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
+
         assert(button.ariaPressed !== undefined);
         assert(button.hasAttribute("aria-pressed"));
     });
 
     it("should define default value", () => {
+        var button = document.createElement("span");
+        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
+
         assert(button.ariaPressed === false);
         assert(button.getAttribute("aria-pressed") === "false");
     });
