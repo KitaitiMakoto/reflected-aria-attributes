adc3cb02bf01d8fb8ea60ad4dd64e2053362a448
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-04-19 07:09:58 +0900

    Add definition for aria-pressed

---
+++ src/reflected-aria-attributes.js
@@ -0,0 +1,29 @@
+"use strict";
+
+export default class ReflectedARIAAttributes {
+	static define(element, attributes) {
+		for (let attrName of attributes) {
+            var desc = this.attributes[attrName];
+            if (! desc) {
+                throw new Error(`Unknown attribute: {$attrName}`);
+            }
+            Object.defineProperty(element, desc.propName, {
+                get: desc.getter,
+                set: desc.setter
+            });
+            element.setAttribute(attrName, desc.default);
+		}
+	}
+}
+ReflectedARIAAttributes.attributes = {
+    "aria-pressed": {
+        propName: "ariaPressed",
+        default: "false",
+        getter: function() {
+            return this.getAttribute("aria-pressed") === "true";
+        },
+        setter: function(value) {
+            this.setAttribute("aria-pressed", !!value);
+        }
+    }
+};
