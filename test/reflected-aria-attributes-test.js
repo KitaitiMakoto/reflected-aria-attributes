ee80a91a471a5430e311e148d4d2f0897e374215
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-04-19 07:20:03 +0900

    Use beforeEach function to setup test fixtures

--- test/reflected-aria-attributes-test.js
+++ test/reflected-aria-attributes-test.js
@@ -8,34 +8,32 @@ var window = jsdom.jsdom().defaultView;
 var document = window.document;
 
 describe("ReflectedARIAAttributes.define()", () => {
-    it("should define reflected WAI-ARIA attributes to an element", () => {
-        var button = document.createElement("span");
+    var button;
+
+    beforeEach(done => {
+        button = document.createElement("span");
         ReflectedARIAAttributes.define(button, ["aria-pressed"]);
 
+        done();
+    });
+
+    it("should define reflected WAI-ARIA attributes to an element", () => {
         assert(button.ariaPressed !== undefined);
         assert(button.hasAttribute("aria-pressed"));
     });
 
     it("should define default value", () => {
-        var button = document.createElement("span");
-        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
-
         assert(button.ariaPressed === false);
         assert(button.getAttribute("aria-pressed") === "false");
     });
 
     it("property change should be reflected to attribute", () => {
-        var button = document.createElement("span");
-        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
         button.ariaPressed = true;
 
         assert(button.getAttribute("aria-pressed") === "true");
     });
 
     it("attribute change should be reflected to property", () => {
-        var button = document.createElement("span");
-        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
-
         button.setAttribute("aria-pressed", "true");
         assert(button.ariaPressed, true);
     });
