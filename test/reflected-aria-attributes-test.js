70a83a7524d5157f70685c707cf1f87b27ae64e9
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-04-19 07:15:52 +0900

    Add tests for reflection

--- test/reflected-aria-attributes-test.js
+++ test/reflected-aria-attributes-test.js
@@ -23,4 +23,20 @@ describe("ReflectedARIAAttributes.define()", () => {
         assert(button.ariaPressed === false);
         assert(button.getAttribute("aria-pressed") === "false");
     });
+
+    it("property change should be reflected to attribute", () => {
+        var button = document.createElement("span");
+        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
+        button.ariaPressed = true;
+
+        assert(button.getAttribute("aria-pressed") === "true");
+    });
+
+    it("attribute change should be reflected to property", () => {
+        var button = document.createElement("span");
+        ReflectedARIAAttributes.define(button, ["aria-pressed"]);
+
+        button.setAttribute("aria-pressed", "true");
+        assert(button.ariaPressed, true);
+    });
 });
